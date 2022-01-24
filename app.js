f9427b5cffa02f5666276c9e3cda826ac0eb9637
Nikolai Petukhov <nikolaipetukhov@gmai.com>
2022-01-24 20:48:08 +0300

    Fix addTask function

--- app.js
+++ app.js
@@ -18,27 +18,27 @@ function loadEventListeners() {
 function addTask(e) {
     if (taskInput.value === '') {
         alert('Add a task')
+    } else {
+        // Create <li>
+        const li = document.createElement('li');
+        li.className = 'collection-item';
+
+        // create text node and append to <li>
+        li.appendChild(document.createTextNode(taskInput.value));
+        // Create new <link> element
+        const link = document.createElement('a');
+        link.className = 'delete-item secondary-content';
+        // add icon html
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        // append the <link> to <li>
+        li.appendChild(link);
+
+        // append <li> to <ul>
+        taskList.appendChild(li);
+
+        // clear input
+        taskInput.value = '';
     }
-
-    // Create <li>
-    const li = document.createElement('li');
-    li.className = 'collection-item';
-
-    // create text node and append to <li>
-    li.appendChild(document.createTextNode(taskInput.value));
-    // Create new <link> element
-    const link = document.createElement('a');
-    link.className = 'delete-item secondary-content';
-    // add icon html
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    // append the <link> to <li>
-    li.appendChild(link);
-
-    // append <li> to <ul>
-    taskList.appendChild(li);
-
-    // clear input
-    taskInput.value = '';
 
     e.preventDefault();
 }
